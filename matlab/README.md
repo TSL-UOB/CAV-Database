589fc147d35a247974b128cf015565b14ad5a1cb
medusa606 <greg.chance@gmail.com>
2021-03-01 14:50:37 +0000

    added readme pics

---
+++ matlab/README.md
@@ -0,0 +1,13 @@
+# CAV-Database
+
+Set the filename of your lanelets data. This must be converted from xml to txt using this method ... Abanoub please add link here! :)
+
+
+Use the plot all function to view all individual lanelets.
+![](lanelet104.jpg)
+
+To plot individual lane ID set 'laneletID' and run 'filter for lanelet ID + plot'
+![](lanelet112.jpg)
+
+To plot a lanelet , which is good for identifying sequential read errors or gaps in the polygon, use 'plot by point'
+
--- matlab/plot_lanelets.m
+++ matlab/plot_lanelets.m
@@ -1,7 +1,7 @@
-% -- select lanelet txt
+%% -- select lanelet txt
 filename = 'loop_geo_RHD.txt';
 
-% -- import to matlab
+%% -- import to matlab
 lanelet = import_lanelet_txt(filename);
 
 %% -- find the unique lanes
@@ -14,12 +14,25 @@ left_x = sel.x(sel.bound=='leftBound',:);
 left_y = sel.y(sel.bound=='leftBound',:);
 righ_x = sel.x(sel.bound=='rightBound',:);
 righ_y = sel.y(sel.bound=='rightBound',:);
-coords = [left_x, left_y;righ_x, righ_y];
 run('plot_func');
+% Coordinates for easy debugging
+coords = [left_x, left_y;flip(righ_x), flip(righ_y); left_x(1), left_y(1)];
 
-%% plot all 
+%% plot by point
+figure(1); clf;hold on
+for index=1:numel(coords(:,1))
+    x = coords(1:index,1);
+    y = coords(1:index,2);
+    
+    plot(x, y, '-k');
+    pause(0.1)
+    plot_title = sprintf('Boundary for lanelet ID %d', laneletID);
+    title(plot_title)
+    legend('outer boundary','location', 'best')
+end
+
+%% plot all
 for index=1:numel(laneIDs)
-
 laneletID = laneIDs(index);
 sel = lanelet(laneletID==lanelet.lanelet_id,:);
 left_x = sel.x(sel.bound=='leftBound',:);
@@ -27,11 +40,6 @@ left_y = sel.y(sel.bound=='leftBound',:);
 righ_x = sel.x(sel.bound=='rightBound',:);
 righ_y = sel.y(sel.bound=='rightBound',:);
 run('plot_func');
-pause(2)
+pause(1)
 end
-
-%% Coordinates for easy debugging
-coords = [left_x, left_y;righ_x, righ_y];
-
-
 
