87deeeb4e75f266d22b97f0a3e401bc5c4e7f085
medusa606 <greg.chance@gmail.com>
2021-03-01 15:44:15 +0000

    added readme pics

--- matlab/plot_lanelets.m
+++ matlab/plot_lanelets.m
@@ -24,15 +24,24 @@ for index=1:numel(coords(:,1))
     x = coords(1:index,1);
     y = coords(1:index,2);    
     plot(x, y, '-k');
-    pause(0.1)
+    xlim([min(coords(:,1)) max(coords(:,1))]);
+    ylim([min(coords(:,2)) max(coords(:,2))]);
+    pause(0.05)
     plot_title = sprintf('Boundary for lanelet ID %d', laneletID);
     title(plot_title)    
-    F(index) = getframe(gcf); %animate    
+    F(index) = getframe(gcf); %animate  
+    if index==numel(coords(:,1)) %persist last frame
+        F(index+1) = F(index);
+        F(index+2) = F(index);
+        F(index+3) = F(index);
+    end
 end
 
 videoTag = strcat('Lanelet_ani_',num2str(laneletID));
 videoFileName = sprintf('%s_%s',videoTag,datestr(now,'HH_MM_SS'));
 exportMovie
+
+
 
 %% plot all
 for index=1:numel(laneIDs)
